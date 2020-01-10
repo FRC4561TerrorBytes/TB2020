7cac8fdcc690736d610782a8d9940a59d5b169b1
Vignesh Balasubramaniam <vigneshb.hp@gmail.com>
2020-01-10 18:10:07 -0500

    New command-based example

--- src/main/java/frc/robot/commands/TeleopDriveCommand.java
+++ src/main/java/frc/robot/commands/TeleopDriveCommand.java
@@ -7,30 +7,38 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.GenericHID.Hand;
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.RobotContainer;
+import frc.robot.subsystems.DriveSubsystem;
 
 public class TeleopDriveCommand extends CommandBase {
+  private DriveSubsystem driveSubsystem;
+  private DoubleSupplier speed;
+  private DoubleSupplier turn_request;
+
   /**
    * Creates a new TeleopDriveCommand.
    */
-  public TeleopDriveCommand() {
-    this.addRequirements(RobotContainer.driveSubsystem);
+  public TeleopDriveCommand(DriveSubsystem driveSubsystem, DoubleSupplier speed, DoubleSupplier turn_request) {
+    this.driveSubsystem = driveSubsystem;
+    this.speed = speed;
+    this.turn_request = turn_request;
+    this.addRequirements(this.driveSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     // Enable/start DriveSubsystem PID loop
-    RobotContainer.driveSubsystem.enable();
+    this.driveSubsystem.enable();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     // Drive PID using speed and turn input from controller for testing
-    RobotContainer.driveSubsystem.teleopPID(RobotContainer.XBOX_CONTROLLER.getY(Hand.kLeft), RobotContainer.XBOX_CONTROLLER.getX(Hand.kRight));
+    this.driveSubsystem.teleopPID(this.speed.getAsDouble(), this.turn_request.getAsDouble());
   }
 
   // Called once the command ends or is interrupted.
